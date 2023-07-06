0b558e3a808e320ef6e7eceb89d899e725d24207
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-07-06 12:51:32 -0700

    validators rules

---
+++ DataFileAnalizer/DataFileAnalizerValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace DataFileAnalizer;
+
+public class DataFileAnalizerValidator : AbstractValidator<DataAnalizer>
+{
+    public DataFileAnalizerValidator()
+    {
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x.path)
+            .Must(File.Exists)
+            .WithMessage("File not exists");
+
+        RuleForEach(x => x.stringArray)
+            .Must(x => int.TryParse(x, out _))
+            .WithMessage("All data must be integer numbers");
+
+        RuleFor(x => x.stringArray)
+            .NotEmpty()
+            .WithMessage("File is empty");
+    }
+}
