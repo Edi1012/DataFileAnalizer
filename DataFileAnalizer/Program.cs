255388b9c511f61f3495d9a04a36889b42de12a2
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-07-06 12:12:42 -0700

    Create host/dependency injection

--- DataFileAnalizer/Program.cs
+++ DataFileAnalizer/Program.cs
@@ -1,7 +1,20 @@
-﻿try
+﻿using DataFileAnalizer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+try
 { 
+    using IHost host = Host
+                        .CreateDefaultBuilder(args)
+                        .ConfigureServices(services => 
+                        { 
+                            services.AddSingleton<Application>();
+                        })
+                        .Build();
 
+                        var application = host.Services.GetRequiredService<Application>();
 
+                        application.Run();
 }
 catch (Exception ex)
 { 
