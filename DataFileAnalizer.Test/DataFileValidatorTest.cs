28645880fd94aabe7026bf047b7a7b8b36dad487
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-07-06 14:00:17 -0700

    add unit test

---
+++ DataFileAnalizer.Test/DataFileValidatorTest.cs
@@ -0,0 +1,89 @@
+﻿using DataFileAnalizer;
+using FluentAssertions;
+
+namespace FileAnalyzer.Tests
+{
+    public class AnalizerValidatorTests
+    {
+        [Fact]
+        public void Validate_WithNonExistingFile_ShouldHaveErrorForPath()
+        {
+            // Arrange
+            var data = new DataAnalizer("FileNoExiste.txt");
+            var validator = new DataFileAnalizerValidator();
+
+            // Act
+            var result = validator.Validate(data);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].ErrorMessage.Should().Be("The file does not exist");
+        }
+
+        [Fact]
+        public void Validate_WithEmptyStringData_ShouldHaveErrorForStringData()
+        {
+            // Arrange
+            //var data = new Data(@"data_empty.txt");
+            var data = new DataAnalizer("C:\\Users\\edgar\\source\\repos\\Edi1012\\FileAnalyzer\\FileAnalyzer.Test\\data_empty.txt");
+            var validator = new DataFileAnalizerValidator();
+
+            // Act
+            var result = validator.Validate(data);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].ErrorMessage.Should().Be("The array cannot be empty");
+        }
+
+        [Fact]
+        public void Validate_WithNonIntegerStringData_ShouldHaveErrorForStringData()
+        {
+            // Arrange
+            var data = new DataAnalizer("C:\\Users\\edgar\\source\\repos\\Edi1012\\FileAnalyzer\\FileAnalyzer.Test\\data_invalid.txt");
+            var validator = new DataFileAnalizerValidator();
+
+            // Act
+            var result = validator.Validate(data);
+
+            // Assert
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].ErrorMessage.Should().Be("All data must be integer numbers");
+        }
+
+        [Fact]
+        public void Print_WithValidData_WritesExpectedOutput()
+        {
+            // Arrange
+            var data = new DataAnalizer("C:\\Users\\edgar\\source\\repos\\Edi1012\\FileAnalyzer\\FileAnalyzer.Test\\data_valid.txt");
+            var expectedOutput = "Total integers: 4\r\n" +
+                                 "Sum: 25\r\n" +
+                                 "Average: 6.25\r\n" +
+                                 "Maximum: 10\r\n" +
+                                 "Minimum: 2\r\n";
+
+            // Act
+            var output = CaptureConsoleOutput(() => data.PrintResults());
+
+            // Assert
+            output.Should().Be(expectedOutput);
+        }
+
+        // Helper method to capture console output
+        private string CaptureConsoleOutput(Action action)
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+
+                action();
+
+                return writer.ToString();
+            }
+        }
+    }
+}
---
+++ DataFileAnalizer.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DataFileAnalizer.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
