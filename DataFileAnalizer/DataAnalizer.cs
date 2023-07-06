3febe435ba44a7370819c77e2dcc0564ca51a195
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-07-06 12:25:33 -0700

    Data File analizer class

---
+++ DataFileAnalizer/DataAnalizer.cs
@@ -0,0 +1,30 @@
+﻿namespace DataFileAnalizer;
+
+public class DataAnalizer
+{
+
+    public DataAnalizer(string path) 
+    {
+        this.path = path;
+    }
+    public string path              { get; set; }
+    private string[] stringArray    { get; set; }
+    private int[] intArray       { get; set; }
+
+
+    public void PrintResults() 
+    {
+        stringArray = File.ReadAllLines(path);
+
+        intArray = stringArray.Select(x => int.Parse(x)).ToArray();
+
+        Console.WriteLine("Total integers:" + intArray.Length);
+        Console.WriteLine("Sum:" + intArray.Sum());
+        Console.WriteLine("Average:" + intArray.Average());
+        Console.WriteLine("Minimum:" + intArray.Min());
+        Console.WriteLine("Minimum:" + intArray.Max());
+    }
+
+
+
+}
